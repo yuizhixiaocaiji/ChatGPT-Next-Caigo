ab64f44a957244634fb694441009b90670d466ef
yizhixiaocaigo <yizhixiaocaigo@163.com>
2024-04-14 09:51:51 +0800

    添加静态资源

--- src/app/components/home.tsx
+++ src/app/components/home.tsx
@@ -1,9 +1,22 @@
 "use client";
 
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import styles from "./home.module.scss";
 import { Path } from "../constant";
 import { SideBar } from "./sideBar";
+import dynamic from "next/dynamic";
+import BotIcon from "../icons/bot.svg";
+import LoadingIcon from "../icons/three-dots.svg";
+
+export function Loading(props: { noLogo?: boolean }) {
+  return (
+    <div className={styles["loading-content"] + " no-dark"}>
+      {!props.noLogo && <BotIcon />}
+      <LoadingIcon />
+    </div>
+  );
+}
+
 function Screen() {
   const isHome = location.pathname === Path.Home;
 
@@ -11,6 +24,16 @@ function Screen() {
     <div className={styles.container}>
       <>
         <SideBar className={isHome ? styles["sidebar-show"] : ""} />
+
+        <div className={styles["window-content"]} id={SlotID.AppBody}>
+          <Routes>
+            <Route path={Path.Home} element={<Chat />} />
+            <Route path={Path.Chat} element={<Chat />} />
+            {/* <Route path={Path.NewChat} element={<NewChat />} /> */}
+            {/* <Route path={Path.Masks} element={<MaskPage />} />
+            <Route path={Path.Settings} element={<Settings />} /> */}
+          </Routes>
+        </div>
       </>
     </div>
   );
